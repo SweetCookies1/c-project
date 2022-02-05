22a2b20616b497edd674633613b768952a6ee75e
SweetCookies1 <andreikovalev233@gmail.com>
2022-02-05 23:49:18 +0300

    memory usage of vector (not ready)

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -11,7 +11,7 @@
 #include "vector.h"
 
 vector createVector(size_t n) {
-    vector v = {(int*) (malloc(sizeof(int) * n)), 0, n};
+    vector v = {(int *) (malloc(sizeof(int) * n)), 0, n};
     if(v.data == NULL) {
         fprintf(stderr, "bad alloc");
         exit(1);
@@ -24,4 +24,18 @@ void reverse(vector *v, size_t newCapacity) {
         v->data = NULL;
     else if (newCapacity < v->size)
         v->size = newCapacity;
+    else
+
+}
+
+void clear(vector *v) {
+    v->size = 0;
+}
+
+void shrinkToFit(vector *v) {
+
+}
+
+void deleteVector(vector *v) {
+    free(v->data);
 }
