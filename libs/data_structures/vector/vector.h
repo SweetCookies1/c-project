e86e5cfd3443e3e0b67f3a3aedc1521d9c29431f
SweetCookies1 <andreikovalev233@gmail.com>
2022-02-05 22:22:43 +0300

    commit2

--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -11,6 +11,31 @@ typedef struct vector {
     int *data;
     size_t size;
     size_t capacity;
-};
+} vector;
 
+vector createVector(size_t n);
+
+void reverse(vector *v, size_t newCapacity);
+
+void clear(vector *v);
+
+void shrinkToFit(vector *v);
+
+void deleteVector(vector *v);
+
+bool isEmpty(vector *v);
+
+bool isFull(vector *v);
+
+int getVectorValue(vector *v, size_t i);
+
+void pushBack(vector *v, int x);
+
+void popBack(vector *v);
+
+int* atVector(vector *v, size_t index);
+
+int* back(vector *v);
+
+int* front(vector *v);
 
